f3632d4da39695a95ff695504ee81c7646e5bf65
DH Shin <dh.shin@3i.ai>
2022-05-25 10:03:26 +0900

    Debug.

--- src/test/java/app/pivotour/dashboard/tests/DashboardLoginTest.java
+++ src/test/java/app/pivotour/dashboard/tests/DashboardLoginTest.java
@@ -38,7 +38,7 @@ public class DashboardLoginTest extends TestBase {
             //Input Email, Password and Click on Login button
             LoginPage login = new LoginPage();
             login.clickLogin(driver);
-            Thread.sleep(2000);
+            Thread.sleep(5000);
             //Check Title
             HeaderPage header = new HeaderPage();
             assertEquals(header.getPageTitle(driver), "Pivo Tour Dashboard");
