663074f0cdaee42a8f39bb24129fe4b7039eea95
DH Shin <dh.shin@3i.ai>
2022-06-23 08:16:28 +0900

    debug

--- src/test/java/app/pivotour/dashboard/tests/TourViewerTest.java
+++ src/test/java/app/pivotour/dashboard/tests/TourViewerTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import static app.pivotour.dashboard.library.ThreadSafeWebDriverStorage.getDriver;
 
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.*;
@@ -43,8 +44,16 @@ public class TourViewerTest extends TestBase {
             tour.click();
             Thread.sleep(5000);
 
-            ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-            driver.switchTo().window(tabs.get(1));
+            //ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+            //driver.switchTo().window(tabs.get(1));
+            String currentHandle= driver.getWindowHandle();
+            Set<String> tabs = driver.getWindowHandles();
+            for(String actual: tabs) {
+                if (!actual.equalsIgnoreCase(currentHandle)) {
+                    //Switch to the opened tab
+                    driver.switchTo().window(actual);
+                }
+            }
 
             TourViewerPage tourView = new TourViewerPage();
             System.out.println(tourView.getPageTitle(driver));
@@ -71,56 +80,63 @@ public class TourViewerTest extends TestBase {
 
 
 
-        @Test(groups = {"debug", "GG"})
-        public void  verifyTourViewerUI2() {
-            WebDriver driver = getDriver();
-            //Go to Tour frontpage (login)
-            //driver.get(config.getString("BASEURL"));
+    @Test(groups = {"debug", "GG"})
+    public void  verifyTourViewerUI2() {
+        WebDriver driver = getDriver();
+        //Go to Tour frontpage (login)
+        //driver.get(config.getString("BASEURL"));
 
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            try {
-                //Input Email, Password and Click on Login button
-                LoginPage login = new LoginPage();
-                login.clickLogin(driver);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try {
+            //Input Email, Password and Click on Login button
+            LoginPage login = new LoginPage();
+            login.clickLogin(driver);
 
-                Thread.sleep(3000);
+            Thread.sleep(3000);
 
-                HeaderPage header = new HeaderPage();
-                //Thread.sleep(3000);
-                header.searchTour2(driver, "For Private Automation Testing");
+            HeaderPage header = new HeaderPage();
+            //Thread.sleep(3000);
+            header.searchTour2(driver, "For Private Automation Testing");
 
-                Thread.sleep(3000);
+            Thread.sleep(3000);
 
-                SearchResultPage search = new SearchResultPage();
-                WebElement tour = search.getTourByTitle(driver, "For Private Automation Testing");
-                tour.click();
-                Thread.sleep(5000);
+            SearchResultPage search = new SearchResultPage();
+            WebElement tour = search.getTourByTitle(driver, "For Private Automation Testing");
+            tour.click();
+            Thread.sleep(5000);
 
-                ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-                driver.switchTo().window(tabs.get(1));
+//            ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+//            driver.switchTo().window(tabs.get(1));
+            String currentHandle= driver.getWindowHandle();
+            Set<String> tabs = driver.getWindowHandles();
+            for(String actual: tabs) {
+                if (!actual.equalsIgnoreCase(currentHandle)) {
+                    //Switch to the opened tab
+                    driver.switchTo().window(actual);
+                }
+            }
 
-                TourViewerPage tourView = new TourViewerPage();
-                System.out.println(tourView.getPageTitle(driver));
-                assertTrue(tourView.getPageTitle(driver).contains("For Private Automation Testing"));
-                //More asserts to be added below
+            TourViewerPage tourView = new TourViewerPage();
+            System.out.println(tourView.getPageTitle(driver));
+            assertTrue(tourView.getPageTitle(driver).contains("For Private Automation Testing"));
+            //More asserts to be added below
 
-                Thread.sleep(3000);
+            Thread.sleep(3000);
 /*
-                ArrayList<String> tabs2 = new ArrayList<>(driver.getWindowHandles());
-                driver.switchTo().window(tabs2.get(1));
+            ArrayList<String> tabs2 = new ArrayList<>(driver.getWindowHandles());
+            driver.switchTo().window(tabs2.get(1));
 */
-                String str = tourView.getPageSource(driver);
-                System.out.println(str);
-                assertTrue(str.contains("For Private Automation Testing"));
+            String str = tourView.getPageSource(driver);
+            System.out.println(str);
+            assertTrue(str.contains("For Private Automation Testing"));
 
-                Thread.sleep(3000);
+            Thread.sleep(3000);
 
-            }
-            catch (Exception e) {
-                e.printStackTrace(System.out);
-                fail("Test Case Failed!");
-            }
         }
-
-
+        catch (Exception e) {
+            e.printStackTrace(System.out);
+            fail("Test Case Failed!");
+        }
     }
+
+}
